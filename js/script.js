67c51d465b02b23f7fb63461a5f3d4fac62a6c39
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-01-14 17:05:38 +0100

    modificato

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 // Un alert espone 5 numeri casuali. Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente. Dopo che
 // sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+var a = numeriRandom(1,100);
+var b = numeriRandom(1,100);
+var c = numeriRandom(1,100);
+var d = numeriRandom(1,100);
+var e = numeriRandom(1,100);
+var numeriGenerati = [a,b,c,d,e];
+var numeriInseriti = [];
+var numeriIndovinati;
+var numeriCorretti = [];
 
-var numeriGenerati = [numeriRandom(1,100),numeriRandom(1,100),numeriRandom(1,100),numeriRandom(1,100),numeriRandom(1,100)];
 alert(numeriGenerati);
 console.log(numeriGenerati);
 
-setTimeout(indovinaNumeri, 1000);
 
+setTimeout(indovinaNumeri, 5000);
 
+var secondi = 5;
+var countdown = setInterval(function(){
+  if (secondi == 0){
+    clearInterval(countdown);
+    document.getElementById('gioco').innerHTML = ' ';
+  } else {
+    document.getElementById('gioco').innerHTML = secondi;
+    --secondi;
+  }
+}, 799);
 
 
 
@@ -17,26 +35,36 @@ function numeriRandom(min, max){
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+
 function indovinaNumeri(){
-  var numeriCorretti = [];
-  for ( var i = 0; i < 5; i++){
 
-    var utente = parseInt(prompt('Inserisci un numero che si trova in un intervallo tra 1 e 100'));
+  while ( numeriInseriti.length != 5 ){
 
-    while ( (utente > 100) || (utente < 0) ){
-      alert('Inserisci un numero tra 1 e 100!!')
-      utente = parseInt(prompt('Inserisci un numero che si trova in un intervallo tra 1 e 100'));
-    }
+          var utente = parseInt(prompt('Inserisci un numero che si trova in un intervallo tra 1 e 100'));
 
-    if ( (numeriGenerati.includes(utente)) && (!numeriCorretti.includes(utente)) ){
-      numeriCorretti.push(utente);
-      var numeriIndovinati = numeriCorretti.length;
-    } else if ( numeriCorretti.length == 0 ){
-      numeriIndovinati = 0;
-      numeriCorretti = 'Mi dispiace non hai indovinato nessun numero..'
-    }
+          if ( numeriInseriti.includes(utente) ){
+            alert('Hai già inserito questo numero!');
+          } else if( utente < 0 || utente > 100 ){
+            alert('iL NUMERO DEVE ESSERE TRA 1 E 100');
+          }
+          else{
+            numeriInseriti.push(utente);
+          }
+        
+          if ( (numeriGenerati.includes(utente) ) && ( !numeriCorretti.includes(utente) ) ){
+            numeriCorretti.push(utente);
+            numeriIndovinati = numeriCorretti.length;
+          } 
+          else if ( numeriCorretti.length == 0 ){
+            numeriIndovinati = 0;
+            numeriCorretti = 'Mi dispiace non hai indovinato nessun numero..'
+          }
 
   }
+
+
+  
   console.log(numeriCorretti);
   document.getElementById('gioco').innerHTML = 'Hai indovinato ' + numeriIndovinati + ' numeri/o! I/Il numeri/o indovinati/o sono/è: ' + numeriCorretti;
+
 }
